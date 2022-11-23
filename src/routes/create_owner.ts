25f6dae927105250c529c20958b3c185ed8da96f
YoussefMohamedSalah <edge.mohamed120@gmail.com>
2022-11-23 04:54:51 +0200

    Owners Route done

--- src/routes/create_owner.ts
+++ src/routes/create_owner.ts
@@ -36,6 +36,54 @@ router.post("/api/create_owner", checkAuth, async (req, res) => {
     return res.json(owner);
 });
 
+// Edit Selected Owner Data
+router.post("/api/edit_owner/:owner_Id", checkAuth, async (req, res) => {
+    const { owner_Id } = req.params;
+    const {
+        first_name,
+        last_name,
+        owner_of,
+        maintenance_fees,
+        car_plate,
+        phone_number,
+    } = req.body;
+
+
+    const owner = await Owner.findOneBy({
+        id: req.params.owner_Id,
+    });
+
+    if (!owner) {
+        return res.json({
+            msg: "client not found",
+        });
+    }
+
+    if(first_name !== ''){
+        owner.first_name = first_name
+    }
+    if(last_name !== ''){
+        owner.last_name = last_name
+    }
+    if(owner_of !== ''){
+        owner.owner_of = owner_of
+    }
+    if(maintenance_fees !== ''){
+        owner.maintenance_fees = maintenance_fees
+    }
+    if(car_plate !== ''){
+        owner.car_plate = car_plate
+    }
+    if(phone_number !== ''){
+        owner.phone_number = phone_number
+    }
+
+
+    await owner.save();
+    console.log(owner)
+    return res.json(owner);
+});
+
 // Get All Owners Data
 router.get("/api/get_owners", checkAuth, async (req, res) => {
     const owner = await Owner.find();
