455c1d7ebbbe6db093b38fd01ca8cd3d32e0070d
YoussefMohamedSalah <edge.mohamed120@gmail.com>
2022-11-29 13:10:43 +0200

    village_Id Added

--- src/routes/create_category.ts
+++ src/routes/create_category.ts
@@ -3,32 +3,50 @@ import { Category } from '../entities/Category';
 import { createQueryBuilder } from 'typeorm';
 import { Item } from '../entities/Item';
 import { checkAuth } from '../../middleware/checkAuth';
+import { Village } from '../entities/village';
 
 const router = express.Router();
 
 // Post Request
-router.post('/api/category', async (req, res) => {
+router.post('/api/:village_Id/category',checkAuth, async (req, res) => {
   const { title, shortcut, section } = req.body;
+
+  const { village_Id } = req.params;
+  const village = await Village.findOne({ where: { id: Number(village_Id) } });
+  if (!village)
+    return res
+      .status(404)
+      .json({ message: 'Village Not Found, Please Enter A Valid Village' });
+
   const category = Category.create({
     title,
     shortcut,
-    section
+    section,
+    village
   });
 
   await category.save();
   return res.json(category);
 });
 
-// Get All Ctegories with items
-router.get('/api/category/items', checkAuth, async (req, res) => {
-  const category = await createQueryBuilder('category')
-    .select('category')
-    .from(Category, 'category')
-    .leftJoinAndSelect('category.items', 'items')
-    .getMany();
+// // Get All Ctegories with items
+// router.get('/api/village_Id/category/', checkAuth, async (req, res) => {
+//     const { village_Id } = req.params;
+//   const village = await Village.findOne({ where: { id: Number(village_Id) } });
+//   if (!village)
+//     return res
+//       .status(404)
+//       .json({ message: 'Village Not Found, Please Enter A Valid Village' });
 
-  return res.json(category);
-});
+  
+//     const category = await createQueryBuilder('category')
+//     .select('category')
+//     .from(Category, 'category')
+//     .leftJoinAndSelect('category.items', 'items')
+//     .getMany();
+
+//   return res.json(category);
+// });
 
 // Get single category with items
 router.get('/api/:category_title/items', checkAuth, async (req, res) => {
--- src/routes/create_item.ts
+++ src/routes/create_item.ts
@@ -1,37 +1,72 @@
-import express from "express";
-import { Item } from "../entities/Item";
-import { createQueryBuilder } from "typeorm";
-import { checkAuth } from "../../middleware/checkAuth";
+import express from 'express';
+import { Item } from '../entities/Item';
+import { createQueryBuilder } from 'typeorm';
+import { checkAuth } from '../../middleware/checkAuth';
+import { Category } from '../entities/Category';
+import { Village } from '../entities/village';
 const router = express.Router();
 
-router.post("/api/item", async (req, res) => {
-    const { title, label, condition, category_Id } = req.body;
-    const item = Item.create({
-        title,
-        label,
-        condition,
-        category: category_Id,
-    });
+router.post('/api/:category_Id/create_item',checkAuth, async (req, res) => {
+  const { title, label, condition } = req.body;
+  const { category_Id } = req.params;
 
-    await item.save();
-    return res.json(item);
+  const category = await Category.findOne({
+    where: { id: Number(category_Id) }
+  });
+  if (!category)
+    return res
+      .status(404)
+      .json({ message: 'Category Not Found, Please Enter A Valid category' });
+
+  const item = Item.create({
+    title,
+    label,
+    condition,
+    category
+  });
+
+  await item.save();
+  return res.json(item);
 });
 
-// Get All Items
-router.get("/api/item", async (req, res) => {
-    const item = await Item.find();
-    return res.json(item);
+// Get All Items in One Category After Checking If Village Valid
+router.get('/api/:village_Id/:category_Id/item',checkAuth, async (req, res) => {
+  const { village_Id } = req.params;
+  const { category_Id } = req.params;
+
+  const village = await Village.findOne({ where: { id: Number(village_Id) } });
+  if (!village)
+    return res
+      .status(404)
+      .json({ message: 'Village Not Found, Please Enter A Valid Village' });
+
+  const category = await Category.findOne({
+    where: { id: Number(category_Id) }
+  });
+  if (!category)
+    return res
+      .status(404)
+      .json({ message: 'Category Not Found, Please Enter A Valid category' });
+
+  const villageItems = await createQueryBuilder('category')
+    .select('category')
+    .from(Category, 'category')
+    .where('category.id = :category_Id', { category_Id })
+    .leftJoinAndSelect('category.items', 'items')
+    .getMany();
+
+  return res.json(villageItems);
 });
 
-// Get All Items With Qr Code List
-router.get("/api/all_items/qrcode_list", checkAuth, async (req, res) => {
-    const item = await createQueryBuilder("item")
-        .select("item")
-        .from(Item, "item")
-        .leftJoinAndSelect("item.qrCodeList", "qrCodeList")
-        .getMany();
+// // Get All Items With Qr Code List
+// router.get('/api/all_items/qrcode_list', checkAuth, async (req, res) => {
+//   const item = await createQueryBuilder('item')
+//     .select('item')
+//     .from(Item, 'item')
+//     .leftJoinAndSelect('item.qrCodeList', 'qrCodeList')
+//     .getMany();
 
-    return res.json(item);
-});
+//   return res.json(item);
+// });
 
 export { router as createItemRouter };
