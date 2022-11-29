61909c78f6f1e8632a6dba6fd1b675a394b7af15
YoussefMohamedSalah <edge.mohamed120@gmail.com>
2022-11-29 13:15:54 +0200

    village cheking

--- src/routes/create_village.ts
+++ src/routes/create_village.ts
@@ -1,23 +1,33 @@
-import express from "express";
-import { Village } from "../entities/village";
+import express from 'express';
+import { checkAuth } from '../../middleware/checkAuth';
+import { Village } from '../entities/village';
 const router = express.Router();
 
 // Adding New Village Info
-router.post("/api/create_village", async (req, res) => {
-    const { village_name } = req.body;
-    const village = Village.create({
-        village_name,
-    });
+router.post('/api/create_village', checkAuth, async (req, res) => {
+  const { village_name } = req.body;
 
+  const village = await Village.findOne({ where: { village_name } });
+
+  if (village) {
+    return res.status(404).json({
+      message:
+        'There Is A village With The Enterd Village_Name, Please Enter A New Village Data'
+    });
+  } else {
+    const village = Village.create({
+      village_name
+    });
     await village.save();
-    return res.json(village);
+  }
+
+  return res.json(village);
 });
 
 // Get All Villages Info
-router.get("/api/get_villages", async (req, res) => {
-    const village = await Village.find();
-    return res.json(village);
+router.get('/api/get_villages', checkAuth, async (req, res) => {
+  const village = await Village.find();
+  return res.json(village);
 });
-
 
 export { router as createVillageRouter };
