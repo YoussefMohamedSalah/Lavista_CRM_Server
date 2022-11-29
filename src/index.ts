6cff10196312c8d27c775a8b5913cdd9df43c9a5
YoussefMohamedSalah <edge.mohamed120@gmail.com>
2022-11-29 10:38:17 +0200

    test

--- src/index.ts
+++ src/index.ts
@@ -1,91 +1,91 @@
-import { createConnection } from "typeorm";
-import express from "express";
-import Cors from "cors";
-import dotenv from "dotenv";
+import { createConnection } from 'typeorm';
+import express from 'express';
+import Cors from 'cors';
+import dotenv from 'dotenv';
 // entities
-import { Village } from "./entities/village";
+import { Village } from './entities/village';
 // import { Section } from "./entities/Section";
-import { Category } from "./entities/Category";
-import { Item } from "./entities/Item";
-import { User } from "./entities/User";
-import { Worker } from "./entities/Worker";
-import { Owner } from "./entities/Owner";
+import { Category } from './entities/Category';
+import { Item } from './entities/Item';
+import { User } from './entities/User';
+import { Worker } from './entities/Worker';
+import { Owner } from './entities/Owner';
 // routes
-import { createVillageRouter } from "./routes/create_village";
+import { createVillageRouter } from './routes/create_village';
 // import { createSectionRouter } from "./routes/create_section";
-import { createCategoryRouter } from "./routes/create_category";
-import { createItemRouter } from "./routes/create_item";
-import { createOwnerRouter } from "./routes/create_owner";
-import { createUserRouter } from "./routes/create_user";
-import { createWorkerRouter } from "./routes/create_worker";
+import { createCategoryRouter } from './routes/create_category';
+import { createItemRouter } from './routes/create_item';
+import { createOwnerRouter } from './routes/create_owner';
+import { createUserRouter } from './routes/create_user';
+import { createWorkerRouter } from './routes/create_worker';
 // auth
-import { authRouter } from "./routes/auth/auth";
-import { protectedRouter } from "./routes/protected";
-import { BrokenItem } from "./entities/qr_code/broken_items_options";
-import { createBrokenItemRouter } from "./routes/create_broken_item";
-import { MaintenanceTransaction } from "./entities/maintenance/Maintenance_transaction";
-import { createMaintenanceTransactionRouter } from "./routes/maintenance/create_Maintenance_Transaction";
-import { GenerateQrCode } from "./entities/qr_code/generateQrCode";
-import { createImageUploadRouter } from "./routes/uploadFile/uploadImage";
-
+import { authRouter } from './routes/auth/auth';
+import { protectedRouter } from './routes/protected';
+import { BrokenItem } from './entities/qr_code/broken_items_options';
+import { createBrokenItemRouter } from './routes/create_broken_item';
+import { MaintenanceTransaction } from './entities/maintenance/Maintenance_transaction';
+import { createMaintenanceTransactionRouter } from './routes/maintenance/create_Maintenance_Transaction';
+import { GenerateQrCode } from './entities/qr_code/generateQrCode';
+import { createImageUploadRouter } from './routes/uploadFile/uploadImage';
+// some change
 // constants
 dotenv.config();
 const app = express();
 
 const main = async () => {
-    try {
-        const connection = await createConnection({
-            type: "postgres",
-            host: "localhost",
-            port: 5432,
-            username: "postgres",
-            password: "YoussefMai##",
-            database: "lavista_final",
-            entities: [
-                // sections and category
-                // Section,
-                Category,
-                Item,
-                BrokenItem,
-                GenerateQrCode,
-                // ----
-                Village,
-                // workers and Users
-                User,
-                Worker,
-                Owner,
-                MaintenanceTransaction,
-            ],
-            synchronize: true,
-        });
-
-        console.log("You'r Now Cronnected To Database");
-
-        app.use(express.json());
-        app.use(Cors());
+  try {
+    const connection = await createConnection({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'YoussefMai##',
+      database: 'lavista_final',
+      entities: [
+        // sections and category
+        // Section,
+        Category,
+        Item,
+        BrokenItem,
+        GenerateQrCode,
         // ----
-        // app.use(createSectionRouter);
-        app.use(createCategoryRouter);
-        app.use(createItemRouter);
-        app.use(createBrokenItemRouter);
-        // ----
-        app.use(createOwnerRouter);
-        app.use(createUserRouter);
-        app.use(createWorkerRouter);
-        app.use(createVillageRouter);
-        app.use(createImageUploadRouter);
-        // ------
-        app.use(createMaintenanceTransactionRouter);
+        Village,
+        // workers and Users
+        User,
+        Worker,
+        Owner,
+        MaintenanceTransaction
+      ],
+      synchronize: true
+    });
 
-        app.use("/auth", authRouter);
-        app.use(protectedRouter);
+    console.log("You'r Now Cronnected To Database");
 
-        app.listen(8080, () => {
-            console.log("Now running on port 8080");
-        });
-    } catch (error) {
-        console.error(error);
-        throw new Error("Unable To Connect To Database");
-    }
+    app.use(express.json());
+    app.use(Cors());
+    // ----
+    // app.use(createSectionRouter);
+    app.use(createCategoryRouter);
+    app.use(createItemRouter);
+    app.use(createBrokenItemRouter);
+    // ----
+    app.use(createOwnerRouter);
+    app.use(createUserRouter);
+    app.use(createWorkerRouter);
+    app.use(createVillageRouter);
+    app.use(createImageUploadRouter);
+    // ------
+    app.use(createMaintenanceTransactionRouter);
+
+    app.use('/auth', authRouter);
+    app.use(protectedRouter);
+
+    app.listen(8080, () => {
+      console.log('Now running on port 8080');
+    });
+  } catch (error) {
+    console.error(error);
+    throw new Error('Unable To Connect To Database');
+  }
 };
 main();
